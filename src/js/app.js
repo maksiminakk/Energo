d6cb373ac48c6cfefb8e5e127bf37145cd7874b5
Petrov Maksim <petrov.maksim@millennium-falcon.team>
2022-12-14 21:43:32 +0300

    aldlalsd

--- src/js/app.js
+++ src/js/app.js
@@ -222,105 +222,107 @@ window.addEventListener("DOMContentLoaded", function () {
     }
 
 
-    // const popupLinks = document.querySelectorAll('.popup-link');
-    // const body = document.querySelector('body');
-    // const lockPadding = document.querySelectorAll(".lock-padding");
+    const popupLinks = document.querySelectorAll('.popup-link');
+    const body = document.querySelector('body');
+    const lockPadding = document.querySelectorAll(".lock-padding");
 
-    // let unlock = true;
+    let unlock = true;
 
-    // const timeout = 500;
+    const timeout = 500;
 
-    // if (popupLinks.length > 0) {
-    //     for (let index = 0; index < popupLinks.length; index++) {
-    //         const popupLink = popupLinks[index];
-    //         popupLink.addEventListener("click", function (e) {
-    //             const popupName = popupLink.getAttribute('href').replace('#', '');
-    //             const curentPopup = document.getElementById(popupName);
-    //             popupOpen(curentPopup);
-    //             e.preventDefault();
-    //         });
-    //     }
-    // }
+    if (popupLinks.length > 0) {
+        for (let index = 0; index < popupLinks.length; index++) {
+            const popupLink = popupLinks[index];
+            popupLink.addEventListener("click", function (e) {
+                const popupName = popupLink.getAttribute('href').replace('#', '');
+                const curentPopup = document.getElementById(popupName);
+                popupOpen(curentPopup);
+                e.preventDefault();
+            });
+        }
+    }
 
-    // const popupCloseIcon = document.querySelectorAll('.close-popup');
-    // if (popupCloseIcon.length > 0) {
-    //     for (let index = 0; index < popupCloseIcon.length; index++) {
-    //         const el = popupCloseIcon[index];
-    //         el.addEventListener('click', function (e) {
-    //             popupClose(el.closest('.popup'));
-    //             e.preventDefault();
-    //         });
-    //     }
-    // }
+    const popupCloseIcon = document.querySelectorAll('.close-popup');
+    if (popupCloseIcon.length > 0) {
+        for (let index = 0; index < popupCloseIcon.length; index++) {
+            const el = popupCloseIcon[index];
+            el.addEventListener('click', function (e) {
+                popupClose(el.closest('.popup'));
+                e.preventDefault();
+            });
+        }
+    }
 
-    // function popupOpen(curentPopup) {
-    //     if (curentPopup && unlock) {
-    //         const popupActive = document.querySelector('.popup.open');
-    //         if (popupActive) {
-    //             popupClose(popupActive, false);
-    //         } else {
-    //             bodyLock();
-    //         }
-    //         curentPopup.classList.add('open');
-    //         curentPopup.addEventListener("click", function (e) {
-    //             if (!e.target.closest('.popup__content')) {
-    //                 popupClose(e.target.closest('.popup'));
-    //             }
-    //         });
-    //     }
-    // }
+    function popupOpen(curentPopup) {
+        if (curentPopup && unlock) {
+            const popupActive = document.querySelector('.popup.open');
+            if (popupActive) {
+                popupClose(popupActive, false);
+            } else {
+                bodyLock();
+            }
+            curentPopup.classList.add('open');
+            curentPopup.addEventListener("click", function (e) {
+                if (!e.target.closest('.popup__content')) {
+                    popupClose(e.target.closest('.popup'));
+                }
+            });
+        }
+    }
 
-    // function popupClose(popupActive, doUnlock = true) {
-    //     if (unlock) {
-    //         popupActive.classList.remove('open');
-    //         if (doUnlock) {
-    //             bodyUnLock();
-    //         }
-    //     }
-    // }
+    function popupClose(popupActive, doUnlock = true) {
+        if (unlock) {
+            popupActive.classList.remove('open');
+            if (doUnlock) {
+                bodyUnLock();
+            }
+        }
+    }
 
-    // function bodyLock() {
-    //     const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+    function bodyLock() {
+        const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
 
-    //     if (lockPadding.length > 0) {
-    //         for (let index = 0; index < lockPadding.length; index++) {
-    //             const el = lockPadding[index];
-    //             el.style.paddingRight = lockPaddingValue;
-    //         }
-    //     }
-    //     body.style.paddingRight = lockPaddingValue;
-    //     body.classList.add('_lock');
+        if (lockPadding.length > 0) {
+            for (let index = 0; index < lockPadding.length; index++) {
+                const el = lockPadding[index];
+                el.style.paddingRight = lockPaddingValue;
+            }
+        }
+        body.style.paddingRight = lockPaddingValue;
+        body.classList.add('_lock');
 
-    //     unlock = false;
-    //     setTimeout(function () {
-    //         unlock = true;
-    //     }, timeout);
-    // }
+        unlock = false;
+        setTimeout(function () {
+            unlock = true;
+        }, timeout);
+    }
 
-    // function bodyUnLock() {
-    //     setTimeout(function () {
-    //         if (lockPadding.length > 0) {
-    //             for (let index = 0; index < lockPadding.length; index++) {
-    //                 const el = lockPadding[index];
-    //                 el.style.paddingRight = '0px';
-    //             }
-    //         }
-    //         body.style.paddingRight = '0px';
-    //         body.classList.remove('_lock');
-    //     }, timeout);
+    function bodyUnLock() {
+        setTimeout(function () {
+            if (lockPadding.length > 0) {
+                for (let index = 0; index < lockPadding.length; index++) {
+                    const el = lockPadding[index];
+                    el.style.paddingRight = '0px';
+                }
+            }
+            body.style.paddingRight = '0px';
+            body.classList.remove('_lock');
+        }, timeout);
 
-    //     unlock = false;
-    //     setTimeout(function () {
-    //         unlock = true;
-    //     }, timeout);
-    // }
-
-    // document.addEventListener('keydown', function (e) {
-    //     if (e.which === 27) {
-    //         const popupActive = document.querySelector('.popup.open');
-    //         popupClose(popupActive);
-    //     }
-    // });
+        unlock = false;
+        setTimeout(function () {
+            unlock = true;
+        }, timeout);
+    }
+    const popupActive = document.querySelector('.popup.open');
+    if (popupActive) {
+        document.addEventListener('keydown', function (e) {
+            if (e.which === 27) {
+                popupClose(popupActive);
+            }
+        });
+    }
+   
     [].forEach.call(document.querySelectorAll('.tel'), function (input) {
         var keyCode;
 
@@ -647,6 +649,55 @@ window.addEventListener("DOMContentLoaded", function () {
             },
             1500: {
                 slidesPerView: 3.8,
+            }
+        },
+        watchSlidesProgress: true,
+        watchSlidesVisibility: true,
+        observer: true,
+        observeParents: true,
+        observeSlideChildren: true,
+    });
+
+    const faqSlider = new Swiper('.faq-slider__slider ', {
+        sumulateTouch: false,
+        touchRatio: 1,
+        touchAngel: 45,
+        grabCursor: true,
+        slideToClickedSlide: false,
+        hashNavigation: {
+            watchState: false,
+        },
+        navigation: {
+            nextEl: '.faq__arrow_next',
+            prevEl: '.faq__arrow_prev',
+        },
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+            pageUpDown: true,
+        },
+        slidesPerView: 3,
+        watchoverflow: false,
+        spaceBetween: 16,
+        slidesPerGroup: 1,
+        loopedSlides: 0, 
+        freeMode: false,
+        speed: 600,
+        effect: 'slide',
+        breakpoints: {
+            100: {
+                spaceBetween: 16,
+                slidesPerView: 'auto',
+            },
+            492: {
+
+            },
+            767: {
+                spaceBetween: 16,
+                slidesPerView: 'auto',
+            },
+            1024: {
+                slidesPerView: 3,
             }
         },
         watchSlidesProgress: true,
